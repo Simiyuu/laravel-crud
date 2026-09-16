a103292b3e053638816353bacd30a55e138d1873
Chandler <chandlermxtere@gmail.com>
2026-09-16 09:08:07 +0300

    Initial Laravel CRUD setup with custom layout system

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+@section('pageTitle', 'Home')
+@section('content')
+    <div class="jumbotron">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">Welcome to my CRUD App.</p>
+    </div>    
+@endsection
